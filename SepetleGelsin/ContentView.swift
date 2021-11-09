8b68179ee6b70b1efd305490a427b6b832f563c0
Tolga Kağan <tolga.kagan@hotmail.com>
2021-11-09 19:00:15 +0300

    Initial Commit

---
+++ SepetleGelsin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SepetleGelsin
+//
+//  Created by Tolga Kağan Aysu on 9.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SepetleGelsin/SepetleGelsinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SepetleGelsinApp.swift
+//  SepetleGelsin
+//
+//  Created by Tolga Kağan Aysu on 9.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SepetleGelsinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
