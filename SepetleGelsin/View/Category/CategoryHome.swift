24d67a8848b956940db47647db7044cbd7d4947d
Tolga Kağan <tolga.kagan@hotmail.com>
2021-12-20 17:13:45 +0300

    edit image

--- SepetleGelsin/View/Category/CategoryHome.swift
+++ SepetleGelsin/View/Category/CategoryHome.swift
@@ -9,7 +9,7 @@ struct CategoryHome: View {
         NavigationView {
            
             List {
-                viewModel.productList[5].image
+                Image(systemName: "logo.jpeg")
                     .resizable()
                     .frame(width: getScreen().width  ,height: getScreen().height * 0.2, alignment: .center)
                     .scaledToFill()
