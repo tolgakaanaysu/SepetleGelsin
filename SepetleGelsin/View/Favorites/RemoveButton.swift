6d6db25dc64de791fb84ab8938e59bf71bae6f18
Tolga Kağan <tolga.kagan@hotmail.com>
2021-12-24 19:35:16 +0300

    add Alert

--- SepetleGelsin/View/Favorites/RemoveButton.swift
+++ SepetleGelsin/View/Favorites/RemoveButton.swift
@@ -1,13 +1,22 @@
 import SwiftUI
 
 struct RemoveButton: View {
+    @State private var showingAlert = false
     @EnvironmentObject var viewModel: ProductViewModel
     
     var body: some View {
         Button (action: {
-            self.viewModel.favoriteList.removeAll()
+            showingAlert.toggle()
         }){
             Image(systemName: "trash")
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("UYARI"),
+                  message: Text("Tüm Favoriler Silinecek"),
+                  primaryButton: .destructive(Text("SİL")){
+                self.viewModel.favoriteList.removeAll()
+            },
+                  secondaryButton: .cancel() )
         }
 
     }
