32c1a7c8a917d1e04e55acac61a7c7555d751fc3
BedirhanUlas <94016886+BedirhanUlas@users.noreply.github.com>
2021-12-27 21:44:33 +0300

    SessionUserDetails

---
+++ SepetleGelsin/Model/SessionUserDetails.swift
@@ -0,0 +1,13 @@
+//
+//  SessionUserDetails.swift
+//  SepetleGelsin
+//
+//  Created by Bedirhan Ulas on 22.12.2021.
+//
+
+import Foundation
+
+struct SessionUserDetails {
+    let firstName : String
+    let lastName: String
+}
