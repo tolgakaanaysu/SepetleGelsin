1cd04df6b84d0584ed28a18844a965e49c8ec329
BedirhanUlas <94016886+BedirhanUlas@users.noreply.github.com>
2021-12-27 21:48:59 +0300

    LogoutView

--- SepetleGelsin/View/Menu/LogoutView.swift
+++ SepetleGelsin/View/Menu/LogoutView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct LogoutView: View {
+    
+    @EnvironmentObject var sessionService: SessionServiceImpl
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             VStack(alignment: .leading, spacing: 16) {
-                Text("Ad: <Placeholder>")
-                Text("Soyad: <Placeholder>")
-                Text("E-Mail: <Placeholder>")
+                Text("Ad: \(sessionService.userDetails?.firstName ?? "N/A)")")
+                Text("Soyad: \(sessionService.userDetails?.lastName ?? "N/A)")")
             }
             .frame(height: UIScreen.main.bounds.size.height * 0.65)
             ButtonView(title: "Çıkış Yap")
             {
-                
+                sessionService.logout()
             }
         }
         .padding(.horizontal, 16)
@@ -30,6 +31,7 @@ struct LogoutView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView{
             LogoutView()
+                .environmentObject(SessionServiceImpl())
         }
         
     }
