e8a14f032347bac96d35c449034431b2d9c635c5
BedirhanUlas <94016886+BedirhanUlas@users.noreply.github.com>
2021-12-27 21:45:35 +0300

    sepetlegelsinapp.swift

--- SepetleGelsin/SepetleGelsinApp.swift
+++ SepetleGelsin/SepetleGelsinApp.swift
@@ -6,14 +6,24 @@ import Firebase
 
 struct SepetleGelsinApp: App {
     @StateObject var stateObject = ProductViewModel()
+    @StateObject var sessionservice = SessionServiceImpl()
     
     init () {
         FirebaseApp.configure()
     }
     var body: some Scene {
-        WindowGroup {
+        WindowGroup {/*
            ContentView()
-                .environmentObject(stateObject)
+                .environmentObject(stateObject)*/
+            NavigationView{
+                switch sessionservice.state {
+                case .loggedIn:
+                    LogoutView()
+                        .environmentObject(sessionservice)
+                case .loggedOut:
+                    LoginView()
+                }
+            }
                 
             
         }
