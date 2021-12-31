da851c2420b77d5816aa3e69cdf0281d5e0b4f62
Tolga Kağan <tolga.kagan@hotmail.com>
2021-12-31 23:45:49 +0300

    fixed quantity buttons

--- SepetleGelsin/View/ShoppingCart/ShoppingCartRow.swift
+++ SepetleGelsin/View/ShoppingCart/ShoppingCartRow.swift
@@ -1,38 +1,86 @@
 import SwiftUI
 
 struct ShoppingCartRow: View {
-    @State private var count = 0
+    @EnvironmentObject var viewModel: ProductViewModel
+    @State var quantity = 0
     var product: ProductModel
-    
+
     var body: some View {
         HStack {
+            
             product.image
                 .resizable()
-                .frame(width: 100, height: 100)
+                .frame(width: 120, height: 120, alignment: .center)
+                .cornerRadius(10)
+                .shadow(color: .ourApplicationColor , radius: 3)
+                
+           Spacer()
             
-            VStack {
+            HStack{
                 
-                Text(product.title)
-                    .padding(.bottom)
+                //Minus button
+                Button {
+                    quantity = viewModel.productQuantityDict[product.id] ?? 0
+                    if quantity > 1 {
+                        quantity-=1
+                    }
+                    else {
+                        if let index = viewModel.shoppingCartList.firstIndex(where: {$0.id == product.id }) {
+                            viewModel.shoppingCartList.remove(at: index)
+                        }
+                    }
+                    viewModel.productQuantityDict[product.id] = quantity
+                } label: {
+                    Image(systemName: "minus")
+                        .font(.callout)
+                        .foregroundColor(.white)
+                        .frame(width: 30, height: 30)
+                        .background(Color.ourApplicationColor)
+                        .cornerRadius(10)
+                        .opacity(0.8)
+                        
+                    
+                }
+
                 
-                Text("\(product.price) ₺")
+                //count
+                Text(String(viewModel.productQuantityDict[product.id] ?? 0))
+                    .bold()
+                    .font(.callout)
+                    .foregroundColor(.black)
+                    .frame(width: 30, height: 30)
+                    .background(Color.secondary)
+                    .cornerRadius(10)
+                    .opacity(0.7)
+                
+                //plus button
+                Button {
+                    quantity = viewModel.productQuantityDict[product.id] ?? 0
+                    quantity+=1
+                    viewModel.productQuantityDict[product.id] = quantity
+
+                } label: {
+                    Image(systemName: "plus")
+                        .font(.callout)
+                        .foregroundColor(.white)
+                        .frame(width: 30, height: 30)
+                        .background(Color.ourApplicationColor)
+                        .cornerRadius(10)
+                        .opacity(0.8)
+                }
+
+                
                 
             }
             
-            Spacer()
-            
-            Stepper {
-                Text("\(count)")
-            } onIncrement: {
-                count+=1
-            } onDecrement: {
-                count-=1
-            }
+           
 
             
 
             
-        }.padding()
+        }
+        
+        .padding()
     }
 }
 
