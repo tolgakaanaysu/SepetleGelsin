afc13160b3d9328199ef49eeff8d8926927805fb
Tolga Kağan <tolga.kagan@hotmail.com>
2021-12-31 18:04:55 +0300

    add  shoppingCartList and calculateTotalPrice()

--- SepetleGelsin/ViewModel/ProductViewModel.swift
+++ SepetleGelsin/ViewModel/ProductViewModel.swift
@@ -2,12 +2,38 @@ import Foundation
 
 class ProductViewModel: ObservableObject {
     
+    //Product List...
     @Published var productList: [ProductModel] = DataService().loadData("data.json")
+    
+    //Liked Products...
     @Published var favoriteList =  [ProductModel]()
-    @Published var shoppingCartList = [ProductModel]()
+    
+    //Shopping Cart List...
+    @Published var shoppingCartList: [Int : Int] = [:]
+    
+    //Calculate Total Price...
+    func calculateTotalPrice() -> Double {
+        var totalPrice = 0.0
+        var price = 0.0
+            
+        
+        if !shoppingCartList.isEmpty {
+            productList.forEach { product in
+                shoppingCartList.forEach { key,value in
+                    price = 0
+                    if product.id == key {
+                        price *= product.price * Double(value)
+                    }
+                }
+                totalPrice += price
+            }
+        }
+        
+        return totalPrice
+    }
 
     
-    //Categorize Products
+    //Categorize Products...
     var categories: [String: [ProductModel]] {
             Dictionary(
                 grouping: productList,
