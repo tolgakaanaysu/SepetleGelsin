ae81725ff7217d85e851866a81355159194cc8c3
Tolga Kağan <tolga.kagan@hotmail.com>
2021-12-31 22:43:24 +0300

    fixed AddToCartButton() - Add button actions

--- SepetleGelsin/View/ProductDetails/AddToCartButton.swift
+++ SepetleGelsin/View/ProductDetails/AddToCartButton.swift
@@ -10,30 +10,59 @@ import SwiftUI
 struct AddToCartButton: View {
     @EnvironmentObject var viewModel: ProductViewModel
     @State var productQuantity = 0
-    
-    var productID: Int
+    @State var isContain = false
+    var product: ProductModel
+        
     var body: some View {
+        // 1: check quantity of products in cart
+        // 2: check if Cart is empty
+        // 3: check if product is in Cart
+        
         Button{
-            viewModel.shoppingCartList[productID] = productQuantity
+            productQuantity = viewModel.productQuantityDict[product.id] ?? 0
+            
+            if !viewModel.shoppingCartList.isEmpty {
+                viewModel.shoppingCartList.forEach { listItem in
+                    if product.id == listItem.id  {
+                        isContain = true
+                    }
+                     
+                }
+                if !isContain {
+                    viewModel.shoppingCartList.append(product)
+                }
+                
+            } else {
+                viewModel.shoppingCartList.append(product)
+            }
+            productQuantity+=1
+            viewModel.productQuantityDict[product.id] = productQuantity
+            
         } label: {
-            Text("Sepete Ekle")
+            Text("Sepete Ekle")            
                 .font(.title2 )
                 .bold()
                 .foregroundColor(.white)
                 .padding(.vertical,18)
                 .frame(width: getScreen().width * 0.4,
-                       height: getScreen().height * 0.075,
-                       alignment: .center)
+                        height: getScreen().height * 0.075,
+                        alignment: .center)
                 .background(Color.ourApplicationColor)
-                .cornerRadius(15)
+                .cornerRadius(20)
                 .shadow(color: Color.black.opacity(0.15),
-                        radius: 5, x:10 , y: 5)
-        }
+                            radius: 5, x:10 , y: 5)
+            }
+            
+        
+        
+        
+        
+        
     }
 }
 
 struct AddToCartButton_Previews: PreviewProvider {
     static var previews: some View {
-        AddToCartButton(productID: 5)
+        AddToCartButton(product: ProductViewModel().productList[1])
     }
 }
