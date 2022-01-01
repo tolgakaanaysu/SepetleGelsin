9186db9a0fdd495db90ed32ff40e643ee0de413c
Tolga Kağan <tolga.kagan@hotmail.com>
2022-01-01 04:39:28 +0300

    add badge()

--- SepetleGelsin/View/ContentView.swift
+++ SepetleGelsin/View/ContentView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var viewModel: ProductViewModel
     @State private var selection: Tab = .homePage
-
     enum Tab {
         case homePage
         case searchView
@@ -11,6 +11,7 @@ struct ContentView: View {
         case menuList
         case loginView
     }
+    
     
     var body: some View {
         TabView(selection: $selection) {
@@ -32,12 +33,15 @@ struct ContentView: View {
                     Label("Sepet", systemImage: "cart")
                 }
                 .tag(Tab.cartPage)
+                .badge(viewModel.shoppingCartList.count)
             
             FavoritesList()
                 .tabItem {
                     Label("Favorilerim", systemImage: "heart")
                 }
                 .tag(Tab.favoriteList)
+                .badge(viewModel.favoriteList.count)
+                
             
             
             
